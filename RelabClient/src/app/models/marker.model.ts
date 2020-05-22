103e4cccd2436449e4150fc688a04f954e7e2be3
danieleMoli <molinari.daniele@iisgalvanimi.edu.it>
2020-05-22 08:53:18 +0000

    esercizio 04.2

--- RelabClient/src/app/models/marker.model.ts
+++ RelabClient/src/app/models/marker.model.ts
@@ -1,3 +1,5 @@
+import { Icon } from './icon.model';
+
 export class Marker {
     icon = {}
    //Quando creo un nuovo marker e verifico quale label viene passata al costruttore, se contiene il testo
@@ -6,53 +8,75 @@ export class Marker {
     constructor(public lat: number, public lng: number, public label?: string)
     {
         if (this.label.includes("naturale")) {
-            this.icon = { url: './assets/img/fire-extinguisher-48.ico' };
-             this.label = "";
+          this.icon = new Icon ( './assets/img/fire-extinguisher-48.ico', 24 ); //da modificare per ogni icona
+           this.label = "";
+           return;
         }
+
+
         if(this.label.includes("elettrica"))
         {
-            this.icon = { url: './assets/img/flash-48.ico' };
+            this.icon = new Icon ('./assets/img/flash-48.ico',24 );
              this.label = "";
+             return;
         }
+
+
+
         if(this.label.includes("solide"))
         {
-            this.icon = { url: './assets/img/tree-45-48.ico' };
+            this.icon =new Icon ('./assets/img/tree-45-48.ico',24);
              this.label = "";
+             return;
         }
         if(this.label.includes("Olio"))
         {
-            this.icon = { url: './assets/img/toxic-48.ico' };
+            this.icon =new Icon ('./assets/img/toxic-48.ico',24 );
              this.label = "";
+             return;
         }
+
         if(this.label.includes("Teleriscaldamento"))
         {
-            this.icon = { url: './assets/img/water-48.ico' };
+            this.icon = new Icon ('./assets/img/water-48.ico',24 );
              this.label = "";
+             return;
+
         }
         if(this.label.includes("RSU"))
         {
-            this.icon = { url: './assets/img/trash-48.ico' };
+            this.icon = new Icon ('./assets/img/trash-48.ico',24);
              this.label = "";
+             return;
         }
+
         if(this.label.includes("liquide"))
         {
-            this.icon = { url: './assets/img/smoke-explosion-48.ico' };
+            this.icon = new Icon ('./assets/img/smoke-explosion-48.ico',24);
              this.label = "";
+             return;
         }
+
+
         if(this.label.includes("NULL"))
         {
-            this.icon = { url: './assets/img/sad-48.ico' };
+            this.icon = new Icon ('./assets/img/sad-48.ico',24);
              this.label = "";
+             return;
         }
+
         if(this.label.includes("GPL"))
         {
-            this.icon = { url: './assets/img/gas-pump-48.ico' };
+            this.icon =new Icon ('./assets/img/gas-pump-48.ico',24);
              this.label = "";
+             return;
         }
+
         if(this.label.includes("Gasolio"))
         {
-            this.icon = { url: './assets/img/gas-48.ico' };
+            this.icon = new Icon ( './assets/img/gas-48.ico',24);
              this.label = "";
+             return;
         }
     }
   }
